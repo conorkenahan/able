72c6c4da339224c5c1ef5c30c10a38b5459cd5e9
Conor <67439130+conorkenahan@users.noreply.github.com>
2020-11-05 20:01:51 -0500

    changed map viewport and added markers

--- src/Map/MapContainer.js
+++ src/Map/MapContainer.js
@@ -24,6 +24,14 @@ export class MapContainer extends React.Component {
       });
     }
   };
+
+  handleMapReady(mapProps, map) {
+    map.setOptions({
+      draggableCursor: "default",
+      draggingCursor: "pointer",
+    });
+  }
+
   render() {
     const containerStyle = {
       margin: "auto",
@@ -37,13 +45,50 @@ export class MapContainer extends React.Component {
           className="googleMap"
           google={this.props.google}
           containerStyle={containerStyle}
+          // draggable={false}
+          onReady={this.handleMapReady}
           initialCenter={{
-            lat: 40.718145,
-            lng: -73.9817311,
+            lat: 40.670869,
+            lng: -73.961961,
           }}
-          zoom={13}
+          zoom={14}
           onClick={this.onMapClicked}
         >
+          <Marker
+            onClick={this.onMarkerClick}
+            name={"Prospect Park"}
+            position={{ lat: 40.6602037, lng: -73.9689558 }}
+          />
+          <Marker
+            onClick={this.onMarkerClick}
+            name={"Prospect Park Zoo"}
+            position={{ lat: 40.6657246, lng: -73.9644911 }}
+          />
+          <Marker
+            onClick={this.onMarkerClick}
+            name={"Brooklyn Botanic Garden"}
+            position={{ lat: 40.66951, lng: -73.9625044 }}
+          />
+          <Marker
+            onClick={this.onMarkerClick}
+            name={"Brooklyn Museum"}
+            position={{ lat: 40.6712062, lng: -73.9636306 }}
+          />
+          <Marker
+            onClick={this.onMarkerClick}
+            name={"Chuko"}
+            position={{ lat: 40.6807746, lng: -73.9674534 }}
+          />
+          {/* <Marker
+            onClick={this.onMarkerClick}
+            name={"Maya Taqueria"}
+            position={{ lat: 40.67802449999999, lng: -73.9685058 }}
+          /> */}
+          <Marker
+            onClick={this.onMarkerClick}
+            name={"Brooklyn Public Library - Central Branch"}
+            position={{ lat: 40.67251110000001, lng: -73.9682 }}
+          />
           <InfoWindow
             marker={this.state.activeMarker}
             visible={this.state.showingInfoWindow}
@@ -52,17 +97,6 @@ export class MapContainer extends React.Component {
               <h1>{this.state.selectedPlace.name}</h1>
             </div>
           </InfoWindow>
-          <Marker onClick={this.onMarkerClick} name={"Italian Restaurant"} />
-          <Marker
-            onClick={this.onMarkerClick}
-            name={"Art Museum"}
-            position={{ lat: 40.729145, lng: -73.9817311 }}
-          />
-          <Marker
-            onClick={this.onMarkerClick}
-            name={"Washington Square Park"}
-            position={{ lat: 40.729145, lng: -74 }}
-          />
         </Map>
       </div>
     );
---
+++ src/config.js
@@ -0,0 +1,6 @@
+const config = {
+  PLACES_API_ENDPOINT: `https://maps.googleapis.com/maps/api/place/nearbysearch/json?`,
+  TOKEN_KEY: "able-client-auth-token",
+};
+
+export default config;
