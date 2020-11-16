e3a0e548c38c858a020ad266b39e1caa9f0b1451
Conor <67439130+conorkenahan@users.noreply.github.com>
2020-11-16 17:52:10 -0500

    changed API endpoint to prod

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ const config = {
   NEARBY_PLACES_API_ENDPOINT: `https://maps.googleapis.com/maps/api/place/nearbysearch/json?`,
   PLACE_DETAILS_API_ENDPOINT:
     "https://maps.googleapis.com/maps/api/place/details/json?",
-  ABLE_API_ENDPOINT: "http://localhost:8000/api",
+  ABLE_API_ENDPOINT: "https://blooming-fortress-45366.herokuapp.com/api",
   TOKEN_KEY: "able-client-auth-token",
 };
 
